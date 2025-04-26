e39ed2c1b42575262029a28f0591a49a021ba033
varvaraivanova17 <144918846+varvaraivanova17@users.noreply.github.com>
2025-04-27 02:40:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace EmploymentAIS
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginForm());
+        }
+    }
+}
